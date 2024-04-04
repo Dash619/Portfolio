76096d12b68b96cd943c4ede22824a0edb8a91d5
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-04-04 15:04:36 -0400

    Render App Titles into 2 columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,3 +16,22 @@ with col2:
     I specialize in crafting immersive game experiences, developing innovative software solutions, 
     and ensuring the security and integrity of digital environments."""
     st.info(content)
+
+    content2 = """Discover the applications I've crafted below, each a testament to my journey
+     in blending creativity with technology to solve, entertain, and innovate.  Hello! I'm Dash, 
+     a creative and technically adept game designer, proficient Python programmer,
+    and passionate ethical hacker. With a unique blend of artistic vision and technical prowess, 
+    I specialize in crafting immersive game experiences, developing innovative software solutions, 
+    and ensuring the security and integrity of digital environments"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
