0489028e2176eaf8ee20a2e146d3d7a2d288500c
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-04-04 16:06:50 -0400

    Add  description, images and links

--- main.py
+++ main.py
@@ -25,13 +25,19 @@ with col2:
     and ensuring the security and integrity of digital environments"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"Images/" + row["image"])
+        st.link_button("Source Code", row["url"])
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"Images/" + row["image"])
+        st.link_button("Source Code", row["url"])
